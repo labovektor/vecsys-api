fd978c4413f00756a69b9cbcaddfadab2d77f970
anjarmath <anjarmath@gmail.com>
2024-10-13 21:56:59 +0700

    apa ya

--- entity/participants.entity.go
+++ entity/participants.entity.go
@@ -27,45 +27,6 @@ type Participant struct {
 	UpdatedAt     *time.Time   `json:"updated_at"`
 }
 
-// type Biodata struct {
-// 	Name          string `json:"name,omitempty"`
-// 	Gender        string `json:"gender,omitempty"`
-// 	Phone         string `json:"phone,omitempty"`
-// 	Email         string `json:"email,omitempty"`
-// 	IdNumber      string `json:"id_number,omitempty"`
-// 	IdCardPicture string `json:"id_card_picture,omitempty"`
-// }
-
-// // Define a custom type for the slice of Biodata
-// type BiodataSlice []Biodata
-
-// // Implement the Valuer interface for BiodataSlice
-// func (b *BiodataSlice) Value() (driver.Value, error) {
-// 	// Convert the slice of Biodata to JSON
-// 	if b == nil {
-// 		return nil, nil
-// 	}
-// 	return json.Marshal(b)
-// }
-
-// // Implement the Scanner interface for BiodataSlice
-// func (b *BiodataSlice) Scan(src interface{}) error {
-// 	if src == nil {
-// 		*b = nil
-// 		return nil
-// 	}
-// 	switch data := src.(type) {
-// 	case []byte:
-// 		// Convert the JSON data to a slice of Biodata
-// 		return json.Unmarshal(data, b)
-// 	case string:
-// 		// Convert the JSON string data to a slice of Biodata
-// 		return json.Unmarshal([]byte(data), b)
-// 	default:
-// 		return fmt.Errorf("unsupported type: %T", src)
-// 	}
-// }
-
 type ParticipantLoginReq struct {
 	Email    string `json:"email" form:"email"`
 	Password string `json:"password" form:"password"`
