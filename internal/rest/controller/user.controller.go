1573952660284924f1649c9793ace994b7a49117
Muhamad Bayu Yusuf <bayu190903@gmail.com>
2025-05-16 21:12:21 +0700

    Kelola Peserta

--- internal/rest/controller/user.controller.go
+++ internal/rest/controller/user.controller.go
@@ -1,6 +1,8 @@
 package controller
 
 import (
+	"time"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/labovector/vecsys-api/internal/rest/dto"
 	repository "github.com/labovector/vecsys-api/internal/rest/repository/user"
@@ -75,5 +77,44 @@ func (ac *UserController) GetParticipantState(c *fiber.Ctx) error {
 	return c.Status(fiber.StatusOK).JSON(dto.APIResponse{
 		Status: dto.SuccessStatus,
 		Data:   res,
+	})
+}
+
+func (ac *UserController) GetAllParticipant(c *fiber.Ctx) error {
+	eventId := c.Params("id")
+	participant, err := ac.userRepo.FindAllParticipant(eventId)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(dto.APIResponse{
+			Status: dto.ErrorStatus.WithMessage("Kesalahan saat mengambil data user"),
+		})
+	}
+
+	return c.Status(fiber.StatusOK).JSON(dto.APIResponse{
+		Status: dto.SuccessStatus,
+		Data:   participant,
+	})
+}
+
+func (ac *UserController) VerifyParticipant(c *fiber.Ctx) error {
+	ID := c.Params("id")
+	participant, err := ac.userRepo.FindParticipantById(ID, true)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(dto.APIResponse{
+			Status: dto.ErrorStatus.WithMessage("Kesalahan saat mengambil data user"),
+		})
+	}
+
+	now := time.Now()
+	participant.VerifiedAt = &now
+
+	err = ac.userRepo.UpdateParticipant(ID, participant)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(dto.APIResponse{
+			Status: dto.ErrorStatus.WithMessage("Kesalahan saat memperbarui data user"),
+		})
+	}
+	return c.Status(fiber.StatusOK).JSON(dto.APIResponse{
+		Status: dto.SuccessStatus,
+		Data:   participant,
 	})
 }
