899c806195a21b6eac0db28b282dd7d180285833
anjarhariadi <anjarmath@gmail.com>
2026-07-10 01:27:45 +0700

    chore: use helvetica instead of arial font

--- internal/util/card.util.go
+++ internal/util/card.util.go
@@ -35,12 +35,12 @@ func GenerateCard(participant *entity.Participant) ([]byte, error) {
 	}
 
 	pdf.Ln(50)
-	pdf.SetFont("Arial", "B", 20)
+	pdf.SetFont("Helvetica", "B", 20)
 	pdf.CellFormat(0, 10, "Kartu Peserta", "", 1, "C", false, 0, "")
 	pdf.CellFormat(0, 10, participant.Event.Name, "", 1, "C", false, 0, "")
 	pdf.Ln(10)
 
-	pdf.SetFont("Arial", "", 14)
+	pdf.SetFont("Helvetica", "", 14)
 	pdf.CellFormat(50, 10, "Asal Sekolah", "", 0, "", false, 0, "")
 	pdf.CellFormat(0, 10, ": "+participant.Institution.Name, "", 1, "", false, 0, "")
 
@@ -51,10 +51,10 @@ func GenerateCard(participant *entity.Participant) ([]byte, error) {
 	pdf.CellFormat(0, 10, ": "+participant.Region.Name, "", 1, "", false, 0, "")
 
 	pdf.CellFormat(50, 10, "Nomor Peserta", "", 0, "", false, 0, "")
-	pdf.SetFont("Arial", "B", 14)
+	pdf.SetFont("Helvetica", "B", 14)
 	pdf.CellFormat(0, 10, ": "+participant.Id.String(), "", 1, "", false, 0, "")
 
-	pdf.SetFont("Arial", "", 14)
+	pdf.SetFont("Helvetica", "", 14)
 	pdf.CellFormat(50, 10, "Nama", "", 0, "", false, 0, "")
 	pdf.CellFormat(0, 10, ": "+participant.Name, "", 1, "", false, 0, "")
 
@@ -62,21 +62,21 @@ func GenerateCard(participant *entity.Participant) ([]byte, error) {
 	pdf.CellFormat(0, 10, ": "+participant.Email, "", 1, "", false, 0, "")
 
 	pdf.Ln(5)
-	pdf.SetFont("Arial", "B", 14)
+	pdf.SetFont("Helvetica", "B", 14)
 	pdf.Cell(0, 10, "Anggota:")
 	pdf.Ln(10)
 	for i, m := range *participant.Biodata {
-		pdf.SetFont("Arial", "", 14)
+		pdf.SetFont("Helvetica", "", 14)
 		pdf.CellFormat(0, 8, fmt.Sprintf("%d. %s (%s)", i+1, m.Name, m.IdNumber), "", 1, "", false, 0, "")
 		pdf.Ln(5)
 	}
 
 	pdf.Line(10, pdf.GetY(), 200, pdf.GetY())
 	pdf.Ln(5)
-	pdf.SetFont("Arial", "B", 14)
+	pdf.SetFont("Helvetica", "B", 14)
 	pdf.Cell(0, 10, "Contact Person:")
 	pdf.Ln(10)
-	pdf.SetFont("Arial", "", 14)
+	pdf.SetFont("Helvetica", "", 14)
 	pdf.CellFormat(0, 8, participant.Region.ContactNumber+fmt.Sprintf(" (%s)", participant.Region.ContactName), "", 1, "", false, 0, "")
 
 	var buf bytes.Buffer
